cdb83783f7d0e6a24c89386c433ef26ed9c900eb
data-pup <kevinmartinucr@gmail.com>
2018-02-23 11:56:24 -0800

    Added new files for testing the logic to clone, diff, and patch.

---
+++ test/testClone.test.ts
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import { clone } from './importDependencies';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestClone {
+    @test public placeholderTest() {
+        assert.equal(1, 2, 'alloc tests not implemented');
+    }
+}
---
+++ test/testDiff.test.ts
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import { diff } from './importDependencies';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestDiff {
+    @test public placeholderTest() {
+        assert.equal(1, 2, 'alloc tests not implemented');
+    }
+}
