cc3376688fce5d81d4f0d40164f401306fb11daa
data-pup <kevinmartinucr@gmail.com>
2018-02-23 21:22:51 -0800

    Added test suite scaffolds to each of the diff op classes.

---
+++ test/diffopsTests/testDiffOpAlloc.test.ts
@@ -0,0 +1,11 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+// import { diff } from './importDependencies';
+
+/* tslint:disable-next-line:no-unused-variable */
+@suite class TestDiffOpAlloc {
+
+    @test public placeholderTest() {
+        assert.equal(1, 1);
+    }
+}
