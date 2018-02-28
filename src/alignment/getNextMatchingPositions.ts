554b6d8f5cef9818a4f4470406c3897b61f34c06
data-pup <kevinmartinucr@gmail.com>
2018-02-27 17:57:40 -0800

    Added further tests for identifying matches further down in the base/target arrays.

--- src/alignment/getNextMatchingPositions.ts
+++ src/alignment/getNextMatchingPositions.ts
@@ -2,23 +2,46 @@ export const getNextMatchingPositions = <T>(base:T[], target:T[],
                                             baseIndex:number,
                                             targetIndex:number)
                                            : [number, number] => {
-    assertParametersAreValid(base, target, baseIndex, targetIndex);
+    // If the parameters are not valid, return undefined indices.
+    if (!assertParametersAreValid(base, target, baseIndex, targetIndex)) {
+        return [undefined, undefined];
+    }
 
     // Initialize loop variables, current base/target index and starting element.
-    // const [currBaseIndex, currTargetIndex] = [baseIndex, targetIndex];
-    // const [origBaseElem, origTargetElem] = [base[baseIndex], target[targetIndex]];
-    // const maxLength = (base.length > target.length) ? base.length : target.length;
+    let [baseInBounds, targetInBounds] = [true, true];
+    let [currBaseIndex, currTargetIndex] = [baseIndex, targetIndex];
+    const [origBaseElem, origTargetElem] = [base[baseIndex], target[targetIndex]];
 
-    throw new Error('Not Implemented Yet');
+    // Search the rest of the target array for a value matching the element at
+    // the original base position, and vice versa. Return a match.
+    while (baseInBounds || targetInBounds) {
+        if (baseInBounds && (base[currBaseIndex] == origTargetElem)) {
+            return [currBaseIndex, targetIndex];
+        } else if (targetInBounds && (target[currTargetIndex] == origBaseElem)) {
+            return [baseIndex, currTargetIndex];
+        }
+
+        currBaseIndex++;   // Increment the index variables and update the
+        currTargetIndex++; // out-of-bounds flags' values.
+        [baseInBounds, targetInBounds] = [
+            (currBaseIndex < base.length), (currTargetIndex < target.length),
+        ];
+    }
+
+    // If no match was found by the loop, recurse starting from the next
+    // position in the target and base arrays.
+    return getNextMatchingPositions(base, target, baseIndex + 1, targetIndex + 1);
 };
 
 const assertParametersAreValid = <T>(base:T[], target:T[],
                                      baseIndex:number,
-                                     targetIndex:number) : void => {
+                                     targetIndex:number) : boolean => {
     if ((!base) || (!target) // Arrays must exist.
         || (baseIndex >= base.length) // Base index must be < the base length.
         || (targetIndex >= target.length) // Target index must be < length.
         || (baseIndex < 0) || (targetIndex < 0)) { // Indices must be >= 0.
-        throw new Error('getNextMatchingPositions called with invalid parameters.');
+        return false;
+    } else {
+        return true;
     }
 };
--- test/alignmentTests/testGetNextMatchingPositions.test.ts
+++ test/alignmentTests/testGetNextMatchingPositions.test.ts
@@ -15,4 +15,24 @@ import {
         const expectedMatch:[number, number] = [0, 2];
         assertArraysAreEqual(actualMatch, expectedMatch);
     }
+
+    @test public testMatchIsForwardInBaseArray() {
+        const base = [1, 2, 3];
+        const target = [3];
+        const [currBaseIndex, currTargetIndex] = [0, 0];
+        const actualMatch:[number, number] = getNextMatchingPositions(
+            base, target, currBaseIndex, currTargetIndex);
+        const expectedMatch:[number, number] = [2, 0];
+        assertArraysAreEqual(actualMatch, expectedMatch);
+    }
+
+    @test public testFunctionCanRecurseSuccessfully() {
+        const base = [3, 4, 5];
+        const target = [1, 2, 4];
+        const [currBaseIndex, currTargetIndex] = [0, 0];
+        const actualMatch:[number, number] = getNextMatchingPositions(
+            base, target, currBaseIndex, currTargetIndex);
+        const expectedMatch:[number, number] = [1, 2];
+        assertArraysAreEqual(actualMatch, expectedMatch);
+    }
 }
