f20bcca02b5817b25fd4f6129578980f9180ea71
data-pup <kevinmartinucr@gmail.com>
2018-02-28 09:25:45 -0800

    Added some more code to the temp debugging file for making sure that index ranges are being generated correctly.

--- src/tempDebugging.ts
+++ src/tempDebugging.ts
@@ -1,12 +1,21 @@
-import { getAlignment } from './alignment/getAlignment';
+// import { getAlignment } from './alignment/getAlignment';
+// const testGetAlignment = () => {
+//         const base = [1, 2, 3, 4];
+//         const target = [1, 2, 4];
+//         const actualAlignment:[number, number][] = getAlignment(base, target);
+//         process.stdout.write('Alignment:\n');
+//         for (const elem of actualAlignment) {
+//             process.stdout.write(`\t[${elem[0]}, ${elem[1]}]\n`);
+//         }
+// };
+// testGetAlignment();
 
-const temp = () => {
-        const base = [1, 2, 3, 4];
-        const target = [1, 2, 4];
-        const actualAlignment:[number, number][] = getAlignment(base, target);
-        process.stdout.write('Alignment:\n');
-        for (const elem of actualAlignment) {
-            process.stdout.write(`\t[${elem[0]}, ${elem[1]}]\n`);
-        }
+import { getIndicesInRange } from './alignment/getIndicesInRange';
+const testGetIndicesInRange = () => {
+    const range = getIndicesInRange(0, 5);
+    process.stdout.write('Range:\n');
+    for (const elem of range) {
+        process.stdout.write(`\t${elem}\n`);
+    }
 };
-temp();
+testGetIndicesInRange();
