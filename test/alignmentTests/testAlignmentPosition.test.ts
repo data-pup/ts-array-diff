57cbc389a3aea005fb533bc574c336ed990182b3
data-pup <kevinmartinucr@gmail.com>
2018-03-01 17:49:45 -0800

    Cleaned up some code used to test the alignment position class.

--- test/alignmentTests/testAlignmentPosition.test.ts
+++ test/alignmentTests/testAlignmentPosition.test.ts
@@ -3,7 +3,7 @@ import { suite, test } from 'mocha-typescript';
 import {
     AlignmentPosition,
     arrDiffTuple,
-    // assertArraysAreEqual,
+    assertArraysAreEqual,
 } from '../importDependencies';
 
 /* tslint:disable-next-line:no-unused-variable */
@@ -31,29 +31,28 @@ import {
         ];
         const pos = new AlignmentPosition(arrs);
         const lengths = pos.getLengthTuple();
-        assert.equal(lengths[0], 0);
-        assert.equal(lengths[1], 0);
         const bounds = pos.getBoundsTuple();
-        assert.isFalse(bounds[0]);
-        assert.isFalse(bounds[1]);
+        [0, 1].forEach((i) => {
+            assert.equal(lengths[i], 0);
+            assert.isFalse(bounds[i]);
+        });
     }
 
-    // @test public testPositionAccessorsForValidArray() {
-    //     const base = [1, 2, 3, 4, 5];
-    //     const target = [1, 2, 3, 4, 5];
-    //     const pos = new AlignmentPosition(base, target);
-    //     for (let i = 0; i < base.length; i++) {
-    //         pos.setBasePosition(i);
-    //         assert.equal(pos.getBasePosition(), i);
-    //         pos.setTargetPosition(i);
-    //         assert.equal(pos.getTargetPosition(), i);
-    //         pos.setPositions(i, i);
-    //         assertArraysAreEqual(pos.getPositionTuple(), [i, i]);
-    //     }
-    //     pos.setBasePosition(5);
-    //     assert.isUndefined(pos.getBasePosition());
-    //     pos.setTargetPosition(5);
-    //     assert.isUndefined(pos.getTargetPosition());
-    //     assertArraysAreEqual(pos.getPositionTuple(), [undefined, undefined]);
-    // }
+    @test public testPositionAccessorsForValidArray() {
+        // const base = [1, 2, 3, 4, 5];
+        // const target = [1, 2, 3, 4, 5];
+        const arrs:[number[], number[]] = [
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+        ];
+        const pos = new AlignmentPosition(arrs);
+        const loopMax = arrs[0].length;
+        for (let i = 0; i < loopMax; i++) {
+            pos.setPositions([i, i]);
+            assertArraysAreEqual(pos.getPositionTuple(), [i, i]);
+        }
+        const invalidPosition = loopMax;
+        pos.setPositions([invalidPosition, invalidPosition]);
+        assertArraysAreEqual(pos.getPositionTuple(), [undefined, undefined]);
+    }
 }
