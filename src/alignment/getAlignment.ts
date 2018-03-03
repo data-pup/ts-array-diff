39ed6811990e843e4d4a08b41bcf9aaa4b6d359f
data-pup <kevinmartinucr@gmail.com>
2018-03-03 09:55:07 -0800

    Cleaned up some of the alignment generation code.

--- src/alignment/getAlignment.ts
+++ src/alignment/getAlignment.ts
@@ -1,16 +1,17 @@
 import { getIndicesInRange } from './getIndicesInRange';
 import { getNextMatchingPositions } from './getNextMatchingPositions';
 import { AlignmentPosition } from './position/alignmentPosition';
+import { diffElemTuple } from './position/positionTypes';
 
 export const getAlignment = <T>(base:T[], target:T[]) : [T, T][] => {
     // Initialize an position object and the results array that we will return.
     const position = new AlignmentPosition<T>([base, target]);
-    const results = new Array<[T, T]>();
+    const results:diffElemTuple<T>[] = new Array();
 
     // Process the contents of both arrays. Continue to loop while either
     // index is still within the bounds of its array.
     while (position.somePositionInBounds()) {
-        const [bCurr, tCurr]:[T, T] = position.getCurrentElems();
+        const [bCurr, tCurr]:diffElemTuple<T> = position.getCurrentElems();
         const bothInBounds:boolean = position.bothPositionsInBounds();
 
         // Process matching elements, increment both counters.
@@ -30,21 +31,25 @@ export const getAlignment = <T>(base:T[], target:T[]) : [T, T][] => {
                 const [bNextMatch, tNextMatch] = getNextMatchingPositions(
                     base, target, bIndex, tIndex); // Find the next match.
 
-                const remove:[T, T][] = getIndicesInRange(bIndex, bNextMatch)
-                    .map((val:number) : [T, T] => [base[val], undefined]);
-                results.push(...remove); // Mark items for removal from base.
+                const remove:diffElemTuple<T>[] = // Find items that should be
+                    getIndicesInRange(bIndex, bNextMatch) // removed from the base.
+                    .map((val:number) : diffElemTuple<T> => [base[val], undefined]);
+                results.push(...remove);
 
-                const newItems:[T, T][] = getIndicesInRange(tIndex, tNextMatch)
-                .map((val:number) : [T, T] => [undefined, target[val]]);
-                results.push(...newItems); // Mark items to be added.
+                const newItems:diffElemTuple<T>[] = // Find new items that should be
+                    getIndicesInRange(tIndex, tNextMatch) // added to the base.
+                    .map((val:number) : diffElemTuple<T> => [undefined, target[val]]);
+                results.push(...newItems);
 
                 position.setPositions([bNextMatch, tNextMatch]);
             } else if (bCurr != undefined) {
                 results.push([bCurr, undefined]);
                 position.incrementBasePosition();
-            } else {
+            } else if (tCurr != undefined) {
                 results.push([undefined, tCurr]);
                 position.incrementTargetPosition();
+            } else { // If none of the branches above were taken, throw an error.
+                throw new Error('Unexpected error encountered while finding alignment!');
             }
         }
     }
