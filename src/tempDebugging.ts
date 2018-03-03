cf010abd5bd7b7d6bf1107e908bc0fabee4847af
data-pup <kevinmartinucr@gmail.com>
2018-03-03 13:38:53 -0800

    Added some throwawy code that i can use to check how things are working in my alignment generation code.

--- src/tempDebugging.ts
+++ src/tempDebugging.ts
@@ -1,21 +1,24 @@
-import { getAlignment } from './alignment/getAlignment';
-const testGetAlignment = () => {
-        const base = [2, 3, 3, 4];
-        const target = [1, 2, 3, 4, 5];
-        const actualAlignment:[number, number][] = getAlignment(base, target);
-        process.stdout.write('Alignment:\n');
-        for (const elem of actualAlignment) {
-            process.stdout.write(`\t[${elem[0]}, ${elem[1]}]\n`);
-        }
-};
-testGetAlignment();
-
-// import { getIndicesInRange } from './alignment/getIndicesInRange';
-// const testGetIndicesInRange = () => {
-//     const range = getIndicesInRange(0, 5);
-//     process.stdout.write('Range:\n');
-//     for (const elem of range) {
-//         process.stdout.write(`\t${elem}\n`);
-//     }
+// import { getAlignment } from './alignment/getAlignment';
+// const testGetAlignment = () => {
+//         const base = [2, 3, 3, 4];
+//         const target = [1, 2, 3, 4, 5];
+//         const actualAlignment:[number, number][] = getAlignment(base, target);
+//         process.stdout.write('Alignment:\n');
+//         for (const elem of actualAlignment) {
+//             process.stdout.write(`\t[${elem[0]}, ${elem[1]}]\n`);
+//         }
 // };
-// testGetIndicesInRange();
+// testGetAlignment();
+
+import { AlignmentPosition } from './alignment/position/alignmentPosition';
+const temp = () => {
+    const arrs:[number[], number[]] = [
+        [1, 2, 0],
+        [3, 4, 0],
+    ];
+    const startPos = new AlignmentPosition(arrs);
+    const matchPos:AlignmentPosition<number> = startPos.getNextMatchPosition();
+    const [a, b]:[number, number] = matchPos.getPositionTuple();
+    process.stdout.write(`Best match found at: [${a}, ${b}]`);
+};
+temp();
