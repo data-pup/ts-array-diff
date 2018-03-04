b285dc55d2e6e895b484ebc9ca7b9edd52c0ebb0
data-pup <kevinmartinucr@gmail.com>
2018-03-03 17:39:43 -0800

    Yayyyy! :D The alignment logic passes -all- of the previously used test cases, and even more! This code should also run much faster. Awoooooo!

--- test/alignmentTests/testAlignmentGeneration.test.ts
+++ test/alignmentTests/testAlignmentGeneration.test.ts
@@ -35,4 +35,70 @@ import { assertAlignmentsAreEqual } from '../testUtils/assertAlignmentsAreEqual'
         ];
         assertAlignmentsAreEqual(actualAlignment, expectedAlignment);
     }
+
+    @test public testAlignmentWhereBaseHasHead() {
+        const arrs:arrDiffTuple<number> = [
+            [1, 2],
+            [   2],
+        ];
+        const pos = new AlignmentPosition<number>(arrs);
+        const actualAlignment:[number, number][] = pos.getAlignment();
+        const expectedAlignment:[number, number][] = [
+            [1, undefined],
+            [2, 2],
+        ];
+        assertAlignmentsAreEqual(actualAlignment, expectedAlignment);
+    }
+
+    @test public testAlignmentWhereBaseHasTail() {
+        const arrs:arrDiffTuple<number> = [
+            [1, 2],
+            [1   ],
+        ];
+        const pos = new AlignmentPosition<number>(arrs);
+        const actualAlignment:[number, number][] = pos.getAlignment();
+        const expectedAlignment:[number, number][] = [
+            [1, 1],
+            [2, undefined],
+        ];
+        assertAlignmentsAreEqual(actualAlignment, expectedAlignment);
+    }
+
+    @test public testAlignmentWhereBaseHasExtraMiddleElement() {
+        const arrs:arrDiffTuple<number> = [
+            [1, 2, 3],
+            [1,    3],
+        ];
+        const pos = new AlignmentPosition<number>(arrs);
+        const actualAlignment:[number, number][] = pos.getAlignment();
+        const expectedAlignment:[number, number][] = [
+            [1, 1],
+            [2, undefined],
+            [3, 3],
+        ];
+        assertAlignmentsAreEqual(actualAlignment, expectedAlignment);
+    }
+
+    @test public testAlignmentForBaseAndTargetWithVariousChanges() {
+        const arrs:arrDiffTuple<number> = [
+            [   2, 3, 3, 4,    5, 6, 8   ],
+            [1, 2, 3,    4, 4, 5, 7, 8, 9],
+        ];
+        const pos = new AlignmentPosition<number>(arrs);
+        const actualAlignment:[number, number][] = pos.getAlignment();
+        const expectedAlignment:[number, number][] = [
+            [undefined, 1],
+            [2, 2],
+            [3, 3],
+            [3, undefined],
+            [4, 4],
+            [undefined, 4],
+            [5, 5],
+            [6, undefined],
+            [undefined, 7],
+            [8, 8],
+            [undefined, 9],
+        ];
+        assertAlignmentsAreEqual(actualAlignment, expectedAlignment);
+    }
 }
