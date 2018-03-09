81cffb64215c9d61553205ae4752fceb837829ac
data-pup <kevinmartinucr@gmail.com>
2018-03-09 10:07:26 -0800

    Added message passing for the array comparison test utility. Added some basic test test logic for the index range.

--- test/getAlignmentTests/testGetIndexRange.test.ts
+++ test/getAlignmentTests/testGetIndexRange.test.ts
@@ -1,7 +1,23 @@
-import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
+import { getIndexRange } from '../importDependencies';
+import { assertArraysAreEqual } from '../testUtils/assertArraysAreEqual';
+
+// Test Case: [start, end, array, expected range, test description]
+type GetIndexRangeTestCase<T> = [number, number, T[], number[], string];
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestGetIndexRange {
-    @test public placeholder() { assert.equal(1, 2, 'Not Implemented!'); }
+    private static testCases:GetIndexRangeTestCase<any>[] = [
+        [0, 3, ['hello', 'there', 'world'], [0, 1, 2], 'Valid index range can be created'],
+        [0, 1, [5, 6, 7], [0], 'Test Range inclusivity'],
+    ];
+
+    @test public runTests() {
+        TestGetIndexRange.testCases.forEach(
+            <T>(currTest:GetIndexRangeTestCase<T>) : void => {
+                const [min, max, arr, expectedRange, testDesc]:GetIndexRangeTestCase<T> = currTest;
+                const actualRange:number[] = getIndexRange(min, max, arr);
+                assertArraysAreEqual(actualRange, expectedRange, `Test Failed: ${testDesc}`);
+        });
+    }
 }
--- test/testUtils/assertArraysAreEqual.ts
+++ test/testUtils/assertArraysAreEqual.ts
@@ -1,9 +1,25 @@
 import { assert } from 'chai';
 
-export const assertArraysAreEqual = <T>(a:T[], b:T[]) => {
-    assert.equal(a.length, b.length);
+export const assertArraysAreEqual = <T>(a:T[], b:T[], message?:string) : void => {
+    assertLengthsAreEqual(a, b, message);
     const length = a.length;
     for (let i = 0; i < length; i++) {
-        assert.equal(a[i], b[i]);
+        assertElemsAreEqual(a[i], b[i], message);
+    }
+};
+
+const assertLengthsAreEqual = <T>(a:T[], b:T[], message?:string) : void => {
+    if (message !== undefined) {
+        assert.equal(a.length, b.length, message);
+    } else {
+        assert.equal(a.length, b.length);
+    }
+};
+
+const assertElemsAreEqual = <T>(a:T, b:T, message?:string) : void => {
+    if (message !== undefined) {
+        assert.equal(a, b, message);
+    } else {
+        assert.equal(a, b);
     }
 };
