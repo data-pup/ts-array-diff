ea42d0e6cce89b1feb5c4f4de1ab7a1ef432339c
data-pup <kevinmartinucr@gmail.com>
2018-03-09 08:38:26 -0800

    Removed deprecated temp debugging code.

--- src/tempDebugging.ts
+++ src/tempDebugging.ts
@@ -1,15 +1,3 @@
-// import { getAlignment } from './alignment/getAlignment';
-// const testGetAlignment = () => {
-//         const base = [2, 3, 3, 4];
-//         const target = [1, 2, 3, 4, 5];
-//         const actualAlignment:[number, number][] = getAlignment(base, target);
-//         process.stdout.write('Alignment:\n');
-//         for (const elem of actualAlignment) {
-//             process.stdout.write(`\t[${elem[0]}, ${elem[1]}]\n`);
-//         }
-// };
-// testGetAlignment();
-
 import { AlignmentPosition } from './getAlignment/alignmentPosition';
 const temp = () => {
     const arrs:[number[], number[]] = [
