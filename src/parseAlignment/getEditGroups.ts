1028844e0515952715300d18b5dcf7ab7af861c2
data-pup <kevinmartinucr@gmail.com>
2018-03-10 15:39:11 -0800

    Added some comments to the edit group function.

--- src/parseAlignment/getEditGroups.ts
+++ src/parseAlignment/getEditGroups.ts
@@ -3,10 +3,11 @@ import {
     alignmentSeqElem,
 } from '../alignmentTypes';
 
+// Divide an alignment sequence into edit groups. This helps identify where
+// changes have been made, and where no operations are required.
 export const getEditGroups = <T>(alignment:alignmentSeq<T>)
                                 : alignmentSeq<T>[] => {
     const results:alignmentSeq<T>[] = new Array();
-
     let currGroup:alignmentSeq<T> = new Array();
     let isEditGroup:boolean = undefined;
 
@@ -14,19 +15,20 @@ export const getEditGroups = <T>(alignment:alignmentSeq<T>)
         const isEditElem:boolean = getIsEditElem(currElem);
         if (isEditGroup === undefined) { isEditGroup = isEditElem; }
 
-        if (isEditElem === isEditGroup) {
-            currGroup.push(currElem);
-        } else {
-            results.push(currGroup);
+        if (isEditElem === isEditGroup) { // Add the current element to the
+            currGroup.push(currElem); // current group if it is of the same type.
+        } else { // Push the previous group and create a new one if a new
+            results.push(currGroup); // element type has been encountered.
             currGroup = new Array(...[currElem]);
             isEditGroup = isEditElem;
         }
     }
 
     if (currGroup.length > 0) { results.push(currGroup); }
-    return results;
+    return results; // Add the remaining group if it is not empty and return.
 };
 
+// Identify whether an alignment sequence element corresponds with an edit.
 const getIsEditElem = <T>(elem:alignmentSeqElem<T>) : boolean => {
     switch (elem.elemType) {
         case 'noop':
