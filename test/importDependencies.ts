f8b795f47358b85e1236247746d1beb518621b40
data-pup <kevinmartinucr@gmail.com>
2018-03-10 12:51:31 -0800

    Cleaned up the dependency imports file used by the various test suites.

--- test/importDependencies.ts
+++ test/importDependencies.ts
@@ -1,6 +1,8 @@
+// Test utilities.
 export { assertAlignmentsAreEqual } from './testUtils/assertAlignmentsAreEqual';
 export { assertArraysAreEqual } from './testUtils/assertArraysAreEqual';
 
+// Alignment type aliases.
 export {
     alignmentSequence,
     arrTuple,
@@ -10,14 +12,35 @@ export {
     tupleType,
 } from '../src/alignmentTypes';
 
-export { getEditGroups } from '../src/parseAlignment/alignmentParser';
+// Alignment generation functions.
+export { getAlignment } from '../src/getAlignment/getAlignment';
+export {
+    bothInBounds,
+    checkBounds,
+    someInBounds,
+} from '../src/getAlignment/lib/checkBounds';
+export { getDistance } from '../src/getAlignment/lib/distance';
+export {
+    incrementBase,
+    incrementBaseAndTarget,
+    incrementTarget,
+} from '../src/getAlignment/lib/increment';
+export {
+    invalidIndexRangeError,
+    undefinedParamError,
+} from '../src/getAlignment/lib/errorMessages';
+export { getIndexRange } from '../src/getAlignment/lib/getIndexRange';
+export { getNextMatch } from '../src/getAlignment/lib/getNextMatch';
 
+// Alignment parsing tuple types.
+export { getEditGroups } from '../src/parseAlignment/alignmentParser';
 export {
     getIsEditTupleFlags,
     getTupleTypes,
     invalidTupleErrorMessage,
 } from '../src/parseAlignment/getTupleTypes';
 
+// Array diff operations.
 export {
     PopDiffOp,
     PushDiffOp,
@@ -26,18 +49,10 @@ export {
     runOps,
 } from '../src/diffops';
 
+// Delta encoding functions.
 export {
     alloc,
     clone,
     diff,
     patch,
 } from '../src/index';
-
-// FIXUP: Refactoring work for getAlignment
-export { getAlignment } from '../src/getAlignment/getAlignment';
-export { bothInBounds, checkBounds, someInBounds } from '../src/getAlignment/lib/checkBounds';
-export { getDistance } from '../src/getAlignment/lib/distance';
-export { invalidIndexRangeError, undefinedParamError } from '../src/getAlignment/lib/errorMessages';
-export { getIndexRange } from '../src/getAlignment/lib/getIndexRange';
-export { getNextMatch } from '../src/getAlignment/lib/getNextMatch';
-export { incrementBase, incrementBaseAndTarget, incrementTarget } from '../src/getAlignment/lib/increment';
