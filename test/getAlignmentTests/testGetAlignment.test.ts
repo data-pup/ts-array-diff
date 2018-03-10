b8bd2fe35c658a39acb12d6957450d00b25e9c06
data-pup <kevinmartinucr@gmail.com>
2018-03-10 10:28:19 -0800

    Yay! The new refactor for the alignment generation is now working. Time to move all of the test cases over, and eventually remove the prervious object-oriented implementation altogether.

--- test/getAlignmentTests/testGetAlignment.test.ts
+++ test/getAlignmentTests/testGetAlignment.test.ts
@@ -1,7 +1,34 @@
-import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
+import { alignmentSequence, getAlignment } from '../importDependencies';
+import { assertAlignmentsAreEqual } from '../testUtils/assertAlignmentsAreEqual';
+
+// [ base state, target state, expected alignment sequence, test description ]
+type alignmentTestCase<T> = [T[], T[], alignmentSequence<T>, string];
+// type alignmentTestCase<T> = [T[], T[]];
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestGetAlignment {
-    @test public placeholder() { assert.equal(1, 2, 'Not Implemented!'); }
+
+    private static testCases:alignmentTestCase<any>[] = [
+        [
+            [1   ],
+            [1, 2],
+            [ [1, 1], [undefined, 2] ],
+            'Target state has new tail element',
+        ],
+    ];
+
+    private static assertCorrectAlignmentIsCreated<T>(testCase:alignmentTestCase<T>) : void {
+        const [base, target, expectedAlignment, testDesc] = testCase;
+        const actualAlignment:alignmentSequence<T> = getAlignment(base, target);
+        assertAlignmentsAreEqual(actualAlignment, expectedAlignment,
+                                 `Test Failed: ${testDesc}`);
+    }
+
+    @test public runTests() {
+        TestGetAlignment.testCases.forEach(
+            <T>(i:alignmentTestCase<T>) : void =>
+                TestGetAlignment.assertCorrectAlignmentIsCreated(i),
+        );
+    }
 }
--- test/testUtils/assertAlignmentsAreEqual.ts
+++ test/testUtils/assertAlignmentsAreEqual.ts
@@ -1,10 +1,26 @@
 import { assert } from 'chai';
 import { assertArraysAreEqual } from './assertArraysAreEqual';
 
-export const assertAlignmentsAreEqual = <T>(a:[T, T][], b:[T, T][]) => {
-    assert.equal(a.length, b.length);
+export const assertAlignmentsAreEqual = <T>(a:[T, T][], b:[T, T][], message?:string) : void => {
+    assertLengthsAreEqual(a, b, message);
     const length = a.length;
     for (let i = 0; i < length; i++) {
-        assertArraysAreEqual(a[i], b[i]);
+        assertSequenceElementsAreEqual(a[i], b[i], message);
+    }
+};
+
+const assertLengthsAreEqual = <T>(a:[T, T][], b:[T, T][], message?:string) : void => {
+    if (message !== undefined) {
+        assert.equal(a.length, b.length, message);
+    } else {
+        assert.equal(a.length, b.length);
+    }
+};
+
+const assertSequenceElementsAreEqual = <T>(a:[T, T], b:[T, T], message?:string) : void => {
+    if (message !== undefined) {
+        assertArraysAreEqual(a, b, message);
+    } else {
+        assertArraysAreEqual(a, b);
     }
 };
