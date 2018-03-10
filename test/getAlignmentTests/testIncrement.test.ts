a22a79a44bac856cac2d61308beb68fb9fb537f9
data-pup <kevinmartinucr@gmail.com>
2018-03-10 08:59:00 -0800

    Added a test suite for the increment functions. I could add some more test cases, but I will leave this alone for now.

--- test/getAlignmentTests/testIncrement.test.ts
+++ test/getAlignmentTests/testIncrement.test.ts
@@ -1,7 +1,50 @@
 import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
+import {
+    incrementBase,
+    incrementBaseAndTarget,
+    incrementTarget,
+    indexTuple,
+} from '../importDependencies';
+
+// [ input, expected base result, expected target result, expected both result ]
+type incrementTestCase = [indexTuple, indexTuple, indexTuple, indexTuple, string];
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestIncrement {
-    @test public placeholder() { assert.equal(1, 2, 'Not Implemented!'); }
+
+    private static testCases:incrementTestCase[] = [
+        [ [0, 0], [1, 0], [0, 1], [1, 1], 'Simple Increment Test' ],
+    ];
+
+    private static assertIncrementTestPasses(testCase:incrementTestCase) : void {
+        const [pos, expectedBaseResult, expectedTargetResult,
+               expectedBothResult, testDesc] = testCase;
+        const [actualBaseResult, actualTargetResult, actualBothResult] = [
+            incrementBase(pos), incrementTarget(pos), incrementBaseAndTarget(pos),
+        ];
+
+        const expectedResults:indexTuple[] = [
+            expectedBaseResult, expectedTargetResult, expectedBothResult];
+        const actualResult:indexTuple[] = [
+            actualBaseResult, actualTargetResult, actualBothResult];
+        const errorMessages:string[] = [
+            `incrementBase failed test: ${testDesc}`,
+            `incrementTarget failed test: ${testDesc}`,
+            `incrementBaseAndTarget failed test: ${testDesc}`,
+        ];
+
+        [0, 1, 2].forEach((i) : void =>
+            assert.strictEqual(actualResult[i], expectedResults[i], errorMessages[i]),
+        );
+    }
+
+    @test public runIncrementTests() {
+        assert.equal(1, 2, 'Not Implemented!');
+
+        TestIncrement.testCases.forEach(
+            (currTest:incrementTestCase) : void =>
+                TestIncrement.assertIncrementTestPasses(currTest),
+        );
+    }
 }
