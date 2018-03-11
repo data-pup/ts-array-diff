32dd86558e8b35d8bb30b30f02347551ac2245ee
data-pup <kevinmartinucr@gmail.com>
2018-03-11 10:01:48 -0700

    Added a revised test bench for the pop operation object.

--- test/diffopsTests/testDiffOpPop.test.ts
+++ test/diffopsTests/testDiffOpPop.test.ts
@@ -1,31 +1,49 @@
-import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
 import {
     assertArraysAreEqual,
     PopDiffOp,
+    runOps,
 } from '../importDependencies';
+
+type PopTestCase<T> = {
+    base:T[];
+    expectedResults:T[];
+    ops:PopDiffOp<T>[];
+    desc:string;
+};
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestDiffOpPop {
-    @test public testDefaultPopObject() {
-        const defaultPop = new PopDiffOp();
-        assert.equal(defaultPop.count, 1);
-        assert.equal(defaultPop.type, 'pop');
+
+    private static testCases:PopTestCase<any>[] = [
+        {
+            base:[1, 2, 3],
+            expectedResults:[1, 2],
+            ops:[new PopDiffOp<number>()],
+            desc:'Default pop removes one item.',
+        },
+        {
+            base:[],
+            expectedResults:[],
+            ops:[new PopDiffOp<number>()],
+            desc:'Empty array can be popped without exception.',
+        },
+        {
+            base:[1, 2, 3, 4, 5],
+            expectedResults:[],
+            ops:[new PopDiffOp<number>(5)],
+            desc:'Array can be emptied with pop.',
+        },
+    ];
+
+    private static runTest<T>(testCase:PopTestCase<T>) : void {
+        const {base, expectedResults, ops, desc} = testCase;
+        const actualResults:T[] = new Array(...base);
+        runOps(actualResults, ops);
+        assertArraysAreEqual(actualResults, expectedResults, `Test Failed: ${desc}`);
     }
 
-    @test public testBasicPopOperations() {
-        const arr = [1, 2, 3, 4, 5];
-        const popOps:PopDiffOp<number>[] = [
-            new PopDiffOp(),
-            new PopDiffOp(2),
-        ];
-
-        popOps[0].runOp(arr);
-        let expectedArr = [1, 2, 3, 4];
-        assertArraysAreEqual(arr, expectedArr);
-
-        popOps[1].runOp(arr);
-        expectedArr = [1, 2];
-        assertArraysAreEqual(arr, expectedArr);
+    @test public runTests() {
+        TestDiffOpPop.testCases.forEach((currTest) => TestDiffOpPop.runTest(currTest));
     }
 }
