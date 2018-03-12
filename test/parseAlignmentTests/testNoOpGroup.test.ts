40b2ebbc149ec42436913ae7ee90be8ebc3de479
data-pup <kevinmartinucr@gmail.com>
2018-03-12 12:39:23 -0700

    Added tests for the no op group class. Next is to test that the op group class is functioning as expected.

--- test/parseAlignmentTests/testNoOpGroup.test.ts
+++ test/parseAlignmentTests/testNoOpGroup.test.ts
@@ -1,9 +1,81 @@
 import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
+import { alignmentSeqElem, NoOpGroup } from '../importDependencies';
+
+type NoOpGroupTestCase<T> = {
+    group:alignmentSeqElem<T>[];
+    expectedCount:number;
+    isValid:boolean;
+    testDesc:string;
+};
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestNoOpGroup {
-    @test public placeholder() {
-        assert.equal(1, 2, 'Not Implemented');
+
+    private static readonly testCases:NoOpGroupTestCase<any>[] = [
+        {
+            group:[],
+            expectedCount:undefined,
+            isValid:false,
+            testDesc:'Empty group is not valid.',
+        },
+        {
+            group:[{elemValue:0, elemType:'add'}],
+            expectedCount:undefined,
+            isValid:false,
+            testDesc:'Group containing a add op is not valid.',
+        },
+        {
+            group:[{elemValue:0, elemType:'remove'}],
+            expectedCount:undefined,
+            isValid:false,
+            testDesc:'Group containing a remove op is not valid.',
+        },
+        {
+            group:[{elemValue:0, elemType:'noop'}],
+            expectedCount:1,
+            isValid:true,
+            testDesc:'Group containing a single noop is valid.',
+        },
+        {
+            group:[
+                {elemValue:0, elemType:'noop'},
+                {elemValue:0, elemType:'noop'},
+            ],
+            expectedCount:2,
+            isValid:true,
+            testDesc:'Group containing multiple noops is valid.',
+        },
+        {
+            group:[
+                {elemValue:0, elemType:'add'},
+                {elemValue:0, elemType:'noop'},
+            ],
+            expectedCount:undefined,
+            isValid:false,
+            testDesc:'Group containing a noop and an edit is not valid.',
+        },
+    ];
+
+    private static runTest<T>(testCase:NoOpGroupTestCase<T>) {
+        const {group, expectedCount, isValid, testDesc} = testCase;
+        const failString = `Test Failed: ${testDesc}`;
+        if (isValid) {
+            const testGroup = new NoOpGroup(group);
+            assert.equal(testGroup.type, 'noop', failString);
+            assert.strictEqual(testGroup.count, expectedCount, failString);
+        } else {
+            assert.throws(
+                (items:alignmentSeqElem<T>[]) : void => {
+                    /* tslint:disable-next-line:no-unused-variable */
+                    const invalidGroup = new NoOpGroup(items);
+                },
+                NoOpGroup.noOpGroupGivenInvalidArguments,
+            );
+        }
+    }
+
+    @test public runTests() {
+        TestNoOpGroup.testCases.forEach((currTest) => TestNoOpGroup.runTest(currTest));
     }
 }
--- test/parseAlignmentTests/testOpGroup.test.ts
+++ test/parseAlignmentTests/testOpGroup.test.ts
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
 
 /* tslint:disable-next-line:no-unused-variable */
-@suite class TestNoOpGroup {
+@suite class TestOpGroup {
     @test public placeholder() {
         assert.equal(1, 2, 'Not Implemented');
     }
