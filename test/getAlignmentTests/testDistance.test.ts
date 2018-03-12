f6b3fa18706a1987e6573dbdf362353930d1b3fe
data-pup <kevinmartinucr@gmail.com>
2018-03-12 11:57:35 -0700

    Added some more cleanup to the test suites for alignment sequence generation helper functions.

--- test/getAlignmentTests/testDistance.test.ts
+++ test/getAlignmentTests/testDistance.test.ts
@@ -2,24 +2,41 @@ import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
 import { indexTuple, getDistance } from '../importDependencies';
 
-type DistanceTestCase = [indexTuple, indexTuple, number, string];
+type DistanceTestCase = {
+    startPos:indexTuple;
+    newPos:indexTuple;
+    expectedDistance:number;
+    testDesc:string;
+};
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestDistance {
 
-    private static testCases:DistanceTestCase[] = [
-        [ [0, 0], [0, 1], 1, 'Target incremented by 1 has distance 1' ],
-        [ [0, 0], [1, 0], 1, 'Base incremented by 1 has distance 1' ],
-        [ [0, 0], [1, 1], 2, 'Both incremented by 1 has distance 2' ],
+    private static readonly testCases:DistanceTestCase[] = [
+        {
+            startPos:[0, 0], newPos:[0, 1],
+            expectedDistance:1,
+            testDesc:'Target incremented by 1 has distance 1',
+        },
+        {
+            startPos:[0, 0], newPos:[1, 0],
+            expectedDistance:1,
+            testDesc:'Base incremented by 1 has distance 1',
+        },
+        {
+            startPos:[0, 0], newPos:[1, 1],
+            expectedDistance:2,
+            testDesc:'Both incremented by 1 has distance 2',
+        },
     ];
 
-    @test public basicTests() {
-        TestDistance.testCases.forEach(
-            (currTest:DistanceTestCase) : void => {
-                const [startPos, newPos, expectedDistance, testDesc]
-                    :[indexTuple, indexTuple, number, string] = currTest;
-                const actualDistance = getDistance(startPos, newPos);
-                assert.equal(actualDistance, expectedDistance, `Test Failed: ${testDesc}`);
-        });
+    private static runTest(testCase:DistanceTestCase) {
+        const {startPos, newPos, expectedDistance, testDesc} = testCase;
+        const actualDistance = getDistance(startPos, newPos);
+        assert.equal(actualDistance, expectedDistance, `Test Failed: ${testDesc}`);
+    }
+
+    @test public runTests() {
+        TestDistance.testCases.forEach((currTest) => TestDistance.runTest(currTest));
     }
 }
--- test/getAlignmentTests/testGetIndexRange.test.ts
+++ test/getAlignmentTests/testGetIndexRange.test.ts
@@ -5,19 +5,33 @@ import {
 } from '../importDependencies';
 
 // Test Case: [start, end, array, expected range, test description]
-type GetIndexRangeTestCase = [number, number, number[], string];
+type GetIndexRangeTestCase = {
+    min:number;
+    max:number;
+    expectedRange:number[];
+    testDesc:string;
+};
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestGetIndexRange {
-    private static testCases:GetIndexRangeTestCase[] = [
-        [0, 3, [0, 1, 2], 'Valid index range can be created'],
-        [0, 1, [0], 'Test Range inclusivity'],
+
+    private static readonly testCases:GetIndexRangeTestCase[] = [
+        {
+            min:0, max:3,
+            expectedRange:[0, 1, 2],
+            testDesc:'Valid index range can be created',
+        },
+        {
+            min:0, max:1,
+            expectedRange:[0],
+            testDesc:'Test Range inclusivity',
+        },
     ];
 
     @test public runTests() {
         TestGetIndexRange.testCases.forEach(
             (currTest:GetIndexRangeTestCase) : void => {
-                const [min, max, expectedRange, testDesc]:GetIndexRangeTestCase = currTest;
+                const {min, max, expectedRange, testDesc}:GetIndexRangeTestCase = currTest;
                 const actualRange:number[] = getIndexRange(min, max);
                 assertArraysAreEqual(actualRange, expectedRange, `Test Failed: ${testDesc}`);
         });
