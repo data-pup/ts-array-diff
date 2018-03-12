586cc1fce32450153e37b883e7a93f38ccfeab2b
data-pup <kevinmartinucr@gmail.com>
2018-03-12 11:45:09 -0700

    Cleaned up match identification test suite.

--- test/getAlignmentTests/testGetNextMatch.test.ts
+++ test/getAlignmentTests/testGetNextMatch.test.ts
@@ -5,29 +5,64 @@ import {
     indexTuple,
 } from '../importDependencies';
 
-// [ base state, target state, start pos, expected match, test description ]
-type matchTestCase<T> = [T[], T[], indexTuple, indexTuple, string];
+type matchTestCase<T> = {
+    base:T[];
+    target:T[];
+    pos:indexTuple;
+    expectedMatch:indexTuple;
+    testDesc:string;
+};
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestGetNextMatch {
 
-    private static testCases:matchTestCase<any>[] = [
-        [[1, 2, 0], [3, 4, 0], [0, 0], [2, 2], 'Finding match at tail.'],
-        [[1, 2, 3], [4, 5, 6], [0, 0], [3, 3], 'Lengths returned for no match.'],
-        [[0, 1, 2], [2, 0, 1], [0, 0], [0, 1], 'Closest match is found'],
-        [[1, 2, 3], [1, 2, 3], [0, 0], [0, 0], 'Current pos. is returned if match.'],
-        [[0, 1], [0, 1], [3, 3], [2, 2], 'Lengths returned if out of bounds'],
+    private static readonly testCases:matchTestCase<any>[] = [
+        {
+            base:[1, 2, 0],
+            target:[3, 4, 0],
+            pos:[0, 0],
+            expectedMatch:[2, 2],
+            testDesc:'Finding match at tail.',
+        },
+        {
+            base:[1, 2, 3],
+            target:[4, 5, 6],
+            pos:[0, 0],
+            expectedMatch:[3, 3],
+            testDesc:'Lengths returned for no match.',
+        },
+        {
+            base:[0, 1, 2],
+            target:[2, 0, 1],
+            pos:[0, 0],
+            expectedMatch:[0, 1],
+            testDesc:'Closest match is found',
+        },
+        {
+            base:[1, 2, 3],
+            target:[1, 2, 3],
+            pos:[0, 0],
+            expectedMatch:[0, 0],
+            testDesc:'Current pos. is returned if match.',
+        },
+        {
+            base:[0, 1],
+            target:[0, 1],
+            pos:[3, 3],
+            expectedMatch:[2, 2],
+            testDesc:'Lengths returned if out of bounds',
+        },
     ];
 
-    private static assertTestCasePasses<T>(testCase:matchTestCase<T>) : void {
-        const [base, target, pos, expectedMatch, testDesc]:matchTestCase<T> = testCase;
+    private static runTest<T>(testCase:matchTestCase<T>) : void {
+        const {base, target, pos, expectedMatch, testDesc} = testCase;
         const actualMatch = getNextMatch(pos, [base, target]);
         assertArraysAreEqual(actualMatch, expectedMatch, `Test Failed: ${testDesc}`);
     }
 
     @test public runTests() {
         TestGetNextMatch.testCases.forEach(
-            (currCase) => TestGetNextMatch.assertTestCasePasses(currCase),
+            (currCase) => TestGetNextMatch.runTest(currCase),
         );
     }
 }
