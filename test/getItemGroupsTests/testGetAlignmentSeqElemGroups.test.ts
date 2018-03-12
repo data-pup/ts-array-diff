0c672237310dbe632908640ea9efd185df502db5
data-pup <kevinmartinucr@gmail.com>
2018-03-12 15:40:32 -0700

    Renamed the alignment item grouping function.

--- test/getItemGroupsTests/testGetAlignmentSeqElemGroups.test.ts
+++ test/getItemGroupsTests/testGetAlignmentSeqElemGroups.test.ts
@@ -1,6 +1,6 @@
 import { suite, test } from 'mocha-typescript';
 import { getAlignment } from '../importGetAlignment';
-import { getAlignmentSeqElemGroups } from '../importGetItemGroups';
+import { getItemGroups } from '../importGetItemGroups';
 import { assertGroupsAreSame } from '../importTestUtils';
 import { alignmentSeq } from '../importTypes';
 
@@ -62,7 +62,7 @@ type getSeqElemGroupTestCase<T> = {
     private static runTest<T>(testCase:getSeqElemGroupTestCase<T>) : void {
         const {base, target, expectedGroups, testDesc} = testCase;
         const alignment = getAlignment(base, target);
-        const actualEditGroups = getAlignmentSeqElemGroups(alignment);
+        const actualEditGroups = getItemGroups(alignment);
         assertGroupsAreSame(actualEditGroups, expectedGroups, `Test Failed: ${testDesc}`);
     }
 
--- test/importGetItemGroups.ts
+++ test/importGetItemGroups.ts
@@ -1,4 +1,4 @@
-export { getAlignmentSeqElemGroups } from '../src/getItemGroups/lib/getAlignmentSeqElemGroups';
+export { getItemGroups } from '../src/getItemGroups/lib/getAlignmentSeqElemGroups';
 export { getIsEditElem } from '../src/getItemGroups/lib/isEditElem';
 export { NoOpGroup } from '../src/getItemGroups/lib/noOpGroup';
 export { OpGroup } from '../src/getItemGroups/lib/opGroup';
