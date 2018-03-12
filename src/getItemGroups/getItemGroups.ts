7b8ee5d5907d57e6d691cecbb321a49ff3026d8a
data-pup <kevinmartinucr@gmail.com>
2018-03-12 16:54:46 -0700

    Added some new logic for generating a sequence of item groups.

--- src/getItemGroups/getItemGroups.ts
+++ src/getItemGroups/getItemGroups.ts
@@ -1,8 +1,11 @@
+import { getSeqElemGroups } from './lib/getSeqElemGroups';
 import { itemGroup, itemGroupTag } from './lib/groupTypes';
-// import { getSeqElemGroups } from './lib/getSeqElemGroups';
+import { getIsEditElem } from './lib/isEditElem';
+import { OpGroup } from './lib/opGroup';
+import { NoOpGroup } from './lib/noOpGroup';
 import {
     alignmentSeq,
-    // alignmentSeqElem,
+    alignmentSeqElem,
 } from '../alignmentTypes';
 
 export { itemGroup, itemGroupTag };
@@ -11,6 +14,15 @@ export { itemGroup, itemGroupTag };
 // changes have been made, and where no operations are required.
 export const getItemGroups = <T>(alignment:alignmentSeq<T>)
                                 : itemGroup<T>[] => {
-    // const seqElemGroups:alignmentSeqElem<T>[][] = getSeqElemGroups(alignment);
-    throw new Error('');
+    const seqElemGroups:alignmentSeqElem<T>[][] = getSeqElemGroups(alignment);
+    return seqElemGroups.map((currElems) => createItemGroupFromSeqGroup(currElems));
+};
+
+const createItemGroupFromSeqGroup = <T>(elemGroup:alignmentSeqElem<T>[])
+                                        : itemGroup<T> => {
+    const firstGroupElem:alignmentSeqElem<T> = elemGroup[0];
+    const isEditGroup = getIsEditElem(firstGroupElem);
+    return isEditGroup
+        ? new OpGroup<T>(elemGroup)
+        : new NoOpGroup<T>(elemGroup);
 };
