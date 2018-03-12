d9961158ec20a205fe5b5c0293f6da2b3188615b
data-pup <kevinmartinucr@gmail.com>
2018-03-12 11:27:59 -0700

    Updated the alignment sequence element grouping logic test suite.

--- test/parseAlignmentTests/testGetAlignmentSeqElemGroups.test.ts
+++ test/parseAlignmentTests/testGetAlignmentSeqElemGroups.test.ts
@@ -8,36 +8,64 @@ import {
 
 // [base array, target array, expected edit groups, test description]
 // type getEditGroupTestCase<T> = [T[], T[], alignmentSeq<T>[], string];
-type getEditGroupTestCase<T> = {
+type getSeqElemGroupTestCase<T> = {
     base:T[];
     target:T[];
-    expectedEditGroups:alignmentSeq<T>[];
+    expectedGroups:alignmentSeq<T>[];
     testDesc:string;
 };
 
 /* tslint:disable-next-line:no-unused-variable */
 @suite class TestGetAlignmentSeqElemGroups {
 
-    private static testCases:getEditGroupTestCase<any>[] = [
+    private static testCases:getSeqElemGroupTestCase<any>[] = [
+        {
+            base:[0, 2, 3],
+            target:[1, 2, 3],
+            expectedGroups:[
+                [{elemValue:0, elemType:'remove'}, {elemValue:1, elemType:'add'}],
+                [{elemValue:2, elemType:'noop'}, {elemValue:3, elemType:'noop'}],
+            ],
+            testDesc:'Test shift/unshift sequence element group is identified.',
+        },
+        {
+            base:[1, 2],
+            target:[1, 3],
+            expectedGroups:[
+                [{elemValue:1, elemType:'noop'}],
+                [{elemValue:2, elemType:'remove'}, {elemValue:3, elemType:'add'}],
+            ],
+            testDesc:'Test shift/unshift sequence element group is identified.',
+        },
+        {
+            base:[1, 2, 4],
+            target:[1, 3, 4],
+            expectedGroups:[
+                [{elemValue:1, elemType:'noop'}],
+                [{elemValue:2, elemType:'remove'}, {elemValue:3, elemType:'add'}],
+                [{elemValue:4, elemType:'noop'}],
+            ],
+            testDesc:'Splice sequence element group is identified.',
+        },
         {
             base:[   1, 2, 3, 4, 5],
             target:[0, 1,       4, 5, 6],
-            expectedEditGroups:[
+            expectedGroups:[
                 [{elemValue:0, elemType:'add'}],
                 [{elemValue:1, elemType:'noop'}],
                 [{elemValue:2, elemType:'remove'}, {elemValue:3, elemType:'remove'}],
                 [{elemValue:4, elemType:'noop'}, {elemValue:5, elemType:'noop'}],
                 [{elemValue:6, elemType:'add'}],
             ],
-            testDesc:'Nontrivial Edit Group Test',
+            testDesc:'Nontrivial sequence element group test',
         },
     ];
 
-    private static evaluateTestCase<T>(testCase:getEditGroupTestCase<T>) : void {
-        const {base, target, expectedEditGroups, testDesc} = testCase;
+    private static evaluateTestCase<T>(testCase:getSeqElemGroupTestCase<T>) : void {
+        const {base, target, expectedGroups, testDesc} = testCase;
         const alignment = getAlignment(base, target);
         const actualEditGroups = getAlignmentSeqElemGroups(alignment);
-        assertEditGroupsAreSame(actualEditGroups, expectedEditGroups,
+        assertEditGroupsAreSame(actualEditGroups, expectedGroups,
                                 `Test Failed: ${testDesc}`);
     }
 
@@ -46,40 +74,4 @@ type getEditGroupTestCase<T> = {
             (currTest) => TestGetAlignmentSeqElemGroups.evaluateTestCase(currTest),
         );
     }
-
-    // @test public testSpliceGroupIsIdentified() {
-    //     const base = [1, 2, 4];
-    //     const target = [1, 3, 4];
-    //     const seq:alignmentSeq<number> = getAlignment(base, target);
-    //     const actualGroups:alignmentSeq<number>[] = getEditGroups(seq);
-    //     const expectedGroups:alignmentSeq<number>[] = [
-    //         [ [1, 1] ],
-    //         [ [2, undefined], [undefined, 3] ],
-    //         [ [4, 4] ],
-    //     ];
-    //     this.checkEditGroupsAreSame(actualGroups, expectedGroups);
-    // }
-
-    // @test public testShiftAndUnshiftAreIdentified() {
-    //     const base = [0, 2, 3];
-    //     const target = [1, 2, 3];
-    //     const seq:alignmentSeq<number> = getAlignment(base, target);
-    //     const actualGroups:alignmentSeq<number>[] = getEditGroups(seq);
-    //     const expectedGroups:alignmentSeq<number>[] = [
-    //         [ [0, undefined], [undefined, 1] ],
-    //         [ [2, 2], [3, 3] ],
-    //     ];
-    //     this.checkEditGroupsAreSame(actualGroups, expectedGroups);
-    // }
-
-    // @test public testPopAndPushAreIdentified() {
-    //     const base = [1, 2];
-    //     const target = [1, 3];
-    //     const seq:alignmentSeq<number> = getAlignment(base, target);
-    //     const actualGroups:alignmentSeq<number>[] = getEditGroups(seq);
-    //     const expectedGroups:alignmentSeq<number>[] = [
-    //         [ [1, 1] ], [ [2, undefined], [undefined, 3] ],
-    //     ];
-    //     this.checkEditGroupsAreSame(actualGroups, expectedGroups);
-    // }
 }
