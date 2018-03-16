0bee87df10489dde6441c9d77d669b6022897f50
data-pup <kevinmartinucr@gmail.com>
2018-03-16 14:35:24 -0700

    Added what I think will end up being the top-level diff/patch functions exposed. These need test suites to be implemented next.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 // Implement functions to allocate, clone, diff, and patch an array.
 // -----------------------------------------------------------------
+import { IDiffOp, runOps } from './diffops';
+import { getAlignment } from './getAlignment/getAlignment';
+import { getItemGroups } from './getItemGroups/getItemGroups';
+import { parse } from './parseItemGroups/parseItemGroups';
+
 // Allocate a new array.
 export const alloc = <T>() : T[] => new Array<T>();
 
@@ -13,11 +18,13 @@ export const clone = <T>(input:T[]) : T[] => {
 };
 
 // Define the diff function.
-export const diff = <T>(base:T[], target:T[]) : void => {
-    throw new Error('Not Implemented Yet!');
+export const diff = <T>(base:T[], target:T[]) : IDiffOp<T>[] => {
+    const alignment = getAlignment(base, target);
+    const itemGroups = getItemGroups(alignment);
+    return parse(itemGroups);
 };
 
 // Define the patch function.
-export const patch = <T>(base:T[], p:any) : void => {
-    throw new Error('Not Implemented Yet!');
+export const patch = <T>(base:T[], ops:IDiffOp<T>[]) : void => {
+    runOps(base, ops);
 };
