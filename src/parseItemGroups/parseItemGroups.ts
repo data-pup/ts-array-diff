ed426ff0502e0d3b4fedb90dcebc6022eeed4a45
data-pup <kevinmartinucr@gmail.com>
2018-03-16 11:15:22 -0700

    Added some work on the parse function. The tail of an array can now be processed correctly!.

--- src/parseItemGroups/parseItemGroups.ts
+++ src/parseItemGroups/parseItemGroups.ts
@@ -1,17 +1,54 @@
 import { itemGroup } from '../itemGroupTypes';
-// import {
-//     itemGroupTag,
-//     NoOpGroup,
-//     OpGroup
-// } from '../itemGroupTypes';
+import {
+    // itemGroupTag,
+    NoOpGroup,
+    OpGroup,
+} from '../itemGroupTypes';
 import { IDiffOp } from '../diffops';
-// import {
+import {
     // ShiftDiffOp, UnshiftDiffOp,
     // SpliceDiffOp,
-    // PopDiffOp, PushDiffOp,
-// } from '../diffops';
+    PopDiffOp, PushDiffOp,
+} from '../diffops';
 
 // Parse a sequence of item groups, and return an array of diff operations.
 export const parse = <T>(itemGroups:itemGroup<T>[]) : IDiffOp<T>[] => {
-    throw new Error('Not Implemented Yet!');
+
+    // for each item group
+    // : noop or an op
+    // : what kind of op? construct
+    const ops:IDiffOp<T>[] = new Array();
+
+    /* tslint:disable-next-line:no-unused-variable */
+    let currTargetPosition = 0;
+    for (let i = 0; i < itemGroups.length; i++) {
+        const currGroup = itemGroups[i];
+        switch (currGroup.type) {
+            case 'edit':
+                const currEditGroup = currGroup as OpGroup<T>;
+                if (i === itemGroups.length - 1) {
+                    ops.push(...processTail(currEditGroup));
+                    break;
+                }
+            case 'noop':
+                const currNoopGroup = currGroup as NoOpGroup<T>;
+                currTargetPosition += currNoopGroup.count;
+                break;
+            default:
+                throw new Error('Unexpected group type!');
+        }
+    }
+
+    return ops;
+};
+
+const processTail = <T>(group:OpGroup<T>) : IDiffOp<T>[] => {
+    const results = [];
+    if (group.removeCount > 0) {
+        results.push(new PopDiffOp(group.removeCount));
+    }
+    if (group.addItems.length > 0) {
+        results.push(new PushDiffOp(group.addItems.slice()));
+    }
+    return results;
 };
